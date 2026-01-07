407d98decdf007583e0ef61781cef3ca6142c0e8
Sinch2121 <sinchanasalimath@gmail.com>
2026-01-07 23:35:08 +0530

    Finished the entire project

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import  Hero from './components/Hero.jsx';
 import Cocktails from './components/Cocktails.jsx';
 import About from './components/About.jsx';
 import Art from './components/Art.jsx';
+import Menu from './components/Menu.jsx';
+import Contact from './components/Contact.jsx';
 
 const App = () => {
   return (
@@ -15,8 +17,10 @@ const App = () => {
       <Cocktails/>
       <About />
       <Art />
-
+      <Menu />
+      <Contact />
     </main>
+
   )
 }
 export default App;
