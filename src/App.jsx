b7669612e4026f5b02e0eaf13fbc304fa083e587
Sinch2121 <sinchanasalimath@gmail.com>
2026-01-07 21:00:44 +0530

    Implemented About Section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar.jsx';
 gsap.registerPlugin(ScrollTrigger, SplitText);
 import  Hero from './components/Hero.jsx';
 import Cocktails from './components/Cocktails.jsx';
+import About from './components/About.jsx';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Cocktails/>
+      <About />
 
     </main>
   )
